3a47dd8ad753972970a91b3da99c83b1cb9d32e9
Airshock72 <gelagela2222@gmail.com>
2025-02-12 16:13:44 +0400

    conditionally rendered via roles delete logic

--- src/modules/patients/views/IndexPage.tsx
+++ src/modules/patients/views/IndexPage.tsx
@@ -1,16 +1,15 @@
-import {
-  Button,
-  Form,
-  Table,
-  Tooltip
-} from 'antd'
+import { Button, Form, Table, Tooltip } from 'antd'
 import { DeleteOutlined } from '@ant-design/icons'
 import { FilterFormValues } from 'src/modules/patients/types'
 import usePatients from 'src/modules/patients/hooks/usePatients.ts'
 import { columns, tablePagination } from 'src/modules/patients/helpers'
 import Filters from 'src/modules/patients/views/Filters.tsx'
+import { useAuth } from 'src/providers/AuthProvider.tsx'
+import { UserRole } from 'api/auth/types.ts'
+import { Patient } from 'api/patients/types.ts'
 
 const PatientPage = () => {
+  const auth = useAuth()
   const [form] = Form.useForm<FilterFormValues>()
   const { state, onFinish, handleDelete } = usePatients({ form })
 
@@ -18,22 +17,29 @@ const PatientPage = () => {
     <div className='p-4'>
       <Filters form={form} onFinish={onFinish} />
       <Table
-        columns={[...columns,
-          {
-            title: 'წაშლა',
-            key: 'action',
-            render: (_, record) => (
-              <Tooltip title='წაშლა' placement='top'>
-                <Button
-                  type='link'
-                  danger
-                  onClick={() => handleDelete(record)}
-                  icon={<DeleteOutlined />}
-                  className='delete-button'
-                />
-              </Tooltip>
-            )
-          }]}
+        columns={[
+          ...columns,
+          ...(auth.state.data.user?.roles?.includes(UserRole.ADMIN) ||
+          auth.state.data.user?.roles?.includes(UserRole.ADMIN)
+            ? [{
+              title: 'წაშლა',
+              key: 'action',
+              render: (_: unknown, record: Patient) => (
+                <Tooltip title='წაშლა' placement='top'>
+                  <Button
+                    type='link'
+                    danger
+                    onClick={() =>
+                      handleDelete(record)
+                    }
+                    icon={<DeleteOutlined />}
+                    className='delete-button'
+                  />
+                </Tooltip>
+              )
+            }]
+            : [])
+        ]}
         dataSource={state.data}
         rowKey='key'
         pagination={tablePagination}

171caf391a761ad7253c0e5b6e09e3578937a0ce
Airshock72 <gelagela2222@gmail.com>
2025-02-12 15:57:48 +0400

    separated filter component

---
+++ src/modules/patients/views/Filters.tsx
@@ -0,0 +1,64 @@
+import { Button, Col, DatePicker, Divider, Form, FormInstance, Input, Row, Select, Typography } from 'antd'
+import { FilterFormValues } from 'src/modules/patients/types'
+
+const { RangePicker } = DatePicker
+const { Option } = Select
+const { Title } = Typography
+
+interface FiltersProps {
+    form: FormInstance<FilterFormValues>
+    onFinish: (values: FilterFormValues) => void
+}
+
+const Filters = ({ form, onFinish }: FiltersProps) => {
+  return (
+    <>
+      <Title level={4} className='mb-4'>
+                ფილტრი
+      </Title>
+      <Form form={form} onFinish={onFinish} layout='vertical'>
+        <Row gutter={16} align='middle'>
+          <Col span={4}>
+            <Form.Item name='firstName' label='First Name'>
+              <Input placeholder='სახელი' />
+            </Form.Item>
+          </Col>
+          <Col span={4}>
+            <Form.Item name='lastName' label='Last Name'>
+              <Input placeholder='გვარი' />
+            </Form.Item>
+          </Col>
+          <Col span={4}>
+            <Form.Item
+              name='personalNumber'
+              label='Personal Number'
+            >
+              <Input type='number' placeholder='პირადი ნომერი' />
+            </Form.Item>
+          </Col>
+          <Col span={6}>
+            <Form.Item name='dateRange' label='თარიღი'>
+              <RangePicker style={{ width: '100%' }} />
+            </Form.Item>
+          </Col>
+          <Col span={4}>
+            <Form.Item name='status' label='Status'>
+              <Select placeholder='აირჩიეთ სტატუსი' allowClear>
+                <Option value='1'>აქტიური</Option>
+                <Option value='2'>პასიური</Option>
+              </Select>
+            </Form.Item>
+          </Col>
+          <Col span={2} className='self-end'>
+            <Form.Item>
+              <Button type='primary' htmlType='submit'>ძებნა</Button>
+            </Form.Item>
+          </Col>
+        </Row>
+      </Form>
+      <Divider className='my-14' />
+    </>
+  )
+}
+
+export default Filters
--- src/modules/patients/views/IndexPage.tsx
+++ src/modules/patients/views/IndexPage.tsx
@@ -1,23 +1,14 @@
 import {
   Button,
-  Col,
-  DatePicker,
-  Divider,
   Form,
-  Input,
-  Row,
-  Select,
-  Table, Tooltip,
-  Typography
+  Table,
+  Tooltip
 } from 'antd'
 import { DeleteOutlined } from '@ant-design/icons'
 import { FilterFormValues } from 'src/modules/patients/types'
 import usePatients from 'src/modules/patients/hooks/usePatients.ts'
 import { columns, tablePagination } from 'src/modules/patients/helpers'
-
-const { RangePicker } = DatePicker
-const { Option } = Select
-const { Title } = Typography
+import Filters from 'src/modules/patients/views/Filters.tsx'
 
 const PatientPage = () => {
   const [form] = Form.useForm<FilterFormValues>()
@@ -25,52 +16,7 @@ const PatientPage = () => {
 
   return (
     <div className='p-4'>
-      <Title level={4} className='mb-4'>
-        ფილტრი
-      </Title>
-      <Form form={form} onFinish={onFinish} layout='vertical'>
-        <Row gutter={16} align='middle'>
-          <Col span={4}>
-            <Form.Item name='firstName' label='First Name'>
-              <Input placeholder='სახელი' />
-            </Form.Item>
-          </Col>
-          <Col span={4}>
-            <Form.Item name='lastName' label='Last Name'>
-              <Input placeholder='გვარი' />
-            </Form.Item>
-          </Col>
-          <Col span={4}>
-            <Form.Item
-              name='personalNumber'
-              label='Personal Number'
-            >
-              <Input type='number' placeholder='პირადი ნომერი' />
-            </Form.Item>
-          </Col>
-          <Col span={6}>
-            <Form.Item name='dateRange' label='თარიღი'>
-              <RangePicker style={{ width: '100%' }} />
-            </Form.Item>
-          </Col>
-          <Col span={4}>
-            <Form.Item name='status' label='Status'>
-              <Select placeholder='აირჩიეთ სტატუსი' allowClear>
-                <Option value='1'>აქტიური</Option>
-                <Option value='2'>პასიური</Option>
-              </Select>
-            </Form.Item>
-          </Col>
-          <Col span={2} className='self-end'>
-            <Form.Item>
-              <Button type='primary' htmlType='submit'>ძებნა</Button>
-            </Form.Item>
-          </Col>
-        </Row>
-      </Form>
-
-      <Divider className='my-14' />
-
+      <Filters form={form} onFinish={onFinish} />
       <Table
         columns={[...columns,
           {
